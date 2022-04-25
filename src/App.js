556efe9dcf7116bd944568aaf652ce3d86a3264a
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-04-25 10:47:32 -0400

    poke api fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return null;
+  const URL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=100";
+
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
 }
 
 export default App;
