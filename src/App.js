1aa503749221c1cff86b61c549a02090320c82d8
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-04-27 11:43:01 -0400

    updated file structure

--- src/App.js
+++ src/App.js
@@ -2,19 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const URL = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=100";
-
-  useEffect(() => {
-    fetch(URL)
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  }, []);
-
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
